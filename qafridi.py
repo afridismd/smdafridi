284bd5659fde15044b7313b490d6c18ce7a2dab9
afridismd <41103360+afridismd@users.noreply.github.com>
2018-07-12 11:52:04 +0530

    Create qafridi.py

---
+++ qafridi.py
@@ -0,0 +1,9 @@
+a=int(raw_input())
+b=int(raw_input())
+c=int(raw_input())
+if(a>"b" and a>"c"):
+	print("a is greater")
+elif(b>"c" and b>"a"):	
+	print("b is greater")
+else:
+	print("c is greater")
